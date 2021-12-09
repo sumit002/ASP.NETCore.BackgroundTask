d57ef5353b49b291859b8b3101d80a7a3cdbba9a
Sumit Das <summit.dash01@gmail.com>
2021-12-10 00:09:51 +0530

    comments modified

--- WebAPI.Core.BackgroundTask.API/Startup.cs
+++ WebAPI.Core.BackgroundTask.API/Startup.cs
@@ -27,7 +27,11 @@ namespace WebAPI.Core.BackgroundTask.API
             services.AddSingleton<ISchedulerWork, SchedulerWork>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
